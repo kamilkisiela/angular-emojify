3ec69a7f7f2acc591aa6340ec86d60430ac950e8
Kamil Kisiela <kamilkisiela@users.noreply.github.com>
2017-04-14 15:37:32 +0200

    Add tests (#10)

---
+++ tests/EmojifyPipe.spec.ts
@@ -0,0 +1,31 @@
+import { Pipe } from '@angular/core';
+
+import './__common';
+import { EmojifyPipe } from '../src';
+
+describe('EmojifyPipe', () => {
+  const pipe: Pipe = Reflect.getOwnMetadata('annotations', EmojifyPipe)[0];
+  let emojifyPipe: EmojifyPipe;
+  
+  beforeEach(() => {
+    emojifyPipe = new EmojifyPipe();
+  });
+  
+  it('should has transform method', () => {
+    expect(emojifyPipe.transform).toBeDefined();
+  });
+
+  it('should transform', () => {
+    const text = `It's a :rocket:!`;
+    
+    expect(emojifyPipe.transform(text)).toMatchSnapshot();
+  });
+
+  it('should be named emojify', () => {
+    expect(pipe.name).toBe('emojify');
+  });
+
+  it('should be pure', () => {
+    expect(pipe.pure).toBe(true);
+  });
+});
